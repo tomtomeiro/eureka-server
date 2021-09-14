e12bb307b4e599837840156932d5e9bc50b0821d
Tomas Flores Garcia <tfloresgarcia94@gmail.com>
2021-09-14 15:14:36 -0500

    first commit

---
+++ src/main/java/com/tis/mx/ApplicationStarter.java
@@ -0,0 +1,38 @@
+/* 
+* This program is free software: you can redistribute it and/or modify  
+* it under the terms of the GNU General Public License as published by  
+* the Free Software Foundation, version 3.
+*
+* This program is distributed in the hope that it will be useful, but 
+* WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+* General Public License for more details.
+*
+* Nombre de archivo: ApplicationStarter.java
+* Autor: tflores
+* Fecha de creación: 14 sep. 2021
+*/
+package com.tis.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import com.netflix.discovery.shared.Application;
+
+/**
+ * The Class ApplicationStarter.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ApplicationStarter {
+
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicationStarter.class, args);
+  }
+
+}
